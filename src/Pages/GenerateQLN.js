e45031edca92546428e65edafc4155add8746753
toms-source <gerwincristobal11@gmail.com>
2022-10-24 10:07:15 +0800

    Update

--- src/Pages/GenerateQLN.js
+++ src/Pages/GenerateQLN.js
@@ -1,95 +1,15 @@
 import React from "react";
-import "../Css/GenerateQLN.css";
-import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
+import Tophero from "../Components/Tophero";
+import GenerateForm from "../Components/GenerateForm";
 
 const GenerateQLN = () => {
-  const navigate = useNavigate();
-  const cancel = () => {
-    navigate("/");
-  };
-  const qln = () => {
-    navigate("/generate-qln-success");
-  };
   return (
-    <div className="Generateqln">
-      <div className="headings"></div>
-      <div className="content">
-        <div className="title">Generate Queue Line Number Form</div>
-        <div className="body">
-          <form className="GenerateQLNModalfrm" action="">
-            <div className="input-transactions">
-              <label htmlFor="transactions" className="label">
-                transactions<span className="req">*</span>
-              </label>
-              {/* <input type="text" required placeholder="Change Section" /> */}
-
-              <select
-                name="transactions"
-                id="transactions"
-                required={require}
-                // value={}
-              >
-                <option value="a" disabled selected hidden>
-                  Head of Academic Transactions
-                </option>
-                <option value="accreditation">Accreditation of Subjects</option>
-                <option value="add-change-subjects">
-                  Adding/Changing of Subjects
-                </option>
-                <option value="overload">Overload</option>
-                <option value="request">Online request for petition</option>
-                <option value="cross-enrollment">Cross-Enrollment</option>
-              </select>
-            </div>
-
-            <div className="input-email">
-              <label htmlFor="Email" className="label">
-                Email<span className="req">*</span>
-              </label>
-              <input
-                type="email"
-                required
-                placeholder="Ex. Juan Cruz@yahoo.com"
-                // value={}
-              />
-            </div>
-
-            <div className="input-name">
-              <label htmlFor="Name" className="label">
-                Name<span className="req">*</span>
-              </label>
-              <input
-                type="text"
-                required
-                placeholder="Ex. Juan Cruz"
-                // value={}
-              />
-            </div>
-
-            <div className="generateQLNbtn">
-              <motion.button
-                type="button"
-                // onClick={props.Onclose}
-                onClick={cancel}
-                className="cancel"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                Cancel
-              </motion.button>
-              <motion.button
-                type="button" //yung type gagawin ko muna type="button". Pero type=submit dapat yan
-                className="submit"
-                onClick={qln}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                Submit
-              </motion.button>
-            </div>
-          </form>
-        </div>
+    <div className="generate-queue-line">
+      <div className="header">
+        <Tophero className="tophero" />
+      </div>
+      <div className="body">
+        <GenerateForm className="generateform" />
       </div>
     </div>
   );
--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,15 +1,9 @@
-// import React, { useState } from "react";
-import "../Css/Landing.css";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { FaUserLock as AdminPortal } from "react-icons/fa";
 import Logo from "../Image/seal.png";
-import Line from "../Image/undraw_wait_in_line_o2aq.svg";
-// import NowServingtbl from "../Components/NowServingtbl";
-// import GenerateQLNModal from "../Components/GenerateQLNModal";
 
 const Landing = () => {
-  // const [show, toShow] = useState(false);
   const navigate = useNavigate();
   const adminAuth = () => {
     navigate("/admin");
@@ -18,85 +12,37 @@ const Landing = () => {
     navigate("/generate-qln");
   };
 
-  // const showModal = () => {
-  //   toShow(true);
-  // };
-  // const closeModal = () => {
-  //   toShow(false);
-  // };
   return (
     <div className="landing">
-      {/* <GenerateQLNModal Onclose={closeModal} show={show} /> */}
-
-      <div className="header">
-        <div className="logo">
-          <motion.img
-            src={Logo}
-            alt="PUPLogo"
-            className="landingLogo"
-            animate={{ scale: [1, 2, 2, 1.5, 1], rotate: [0, 0, 270, 270, 0] }}
-          />
-          <div className="pup">
-            <div>pupsmb</div>
-            <div>qms portal</div>
-          </div>
+      <div className="wrapper">
+        <div className="access">
+          <motion.div
+            className="admin"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <abbr title="Admin Access">
+              <AdminPortal onClick={adminAuth} />
+            </abbr>
+          </motion.div>
         </div>
-        <motion.div
-          className="admin"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <abbr title="Admin Access">
-            <AdminPortal onClick={adminAuth} />
-          </abbr>
-        </motion.div>
-      </div>
 
-      <div className="body-section">
-        <div className="body">
-          <div className="element">
-            <div className="tagline">
-              <h2>Office of Academic Head Program</h2>
-            </div>
-            <div className="buttons">
-              <motion.button
-                className="generate"
-                // onClick={showModal}
-                onClick={GenerateQLN}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                Generate Queue Line Number
-              </motion.button>
-
-              <motion.button
-                type="button"
-                className="transaction"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                View My Transaction
-              </motion.button>
-            </div>
-          </div>
-          <img src={Line} alt="" className="imgLine" />
-        </div>
-        <div className="table"></div>
-      </div>
-      {/* <div className="elements">
-          <div className="tagline">be patient, be updated, be inline</div>
+        <div className="content">
+          <div className="bgPup"></div>
 
           <div className="buttons">
+            <div className="floatingLogo">
+              <img src={Logo} alt="" />
+            </div>
+            <h2>Office of Academic Head Program</h2>
             <motion.button
               className="generate"
-              // onClick={showModal}
               onClick={GenerateQLN}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
               Generate Queue Line Number
             </motion.button>
-
             <motion.button
               type="button"
               className="transaction"
@@ -106,10 +52,11 @@ const Landing = () => {
               View My Transaction
             </motion.button>
           </div>
-          {/* <div className="data">
-            <NowServingtbl />
-          </div> 
-        </div> */}
+        </div>
+        <div className="tables">
+          <h2>QUEUELINE HERE</h2>
+        </div>
+      </div>
     </div>
   );
 };
